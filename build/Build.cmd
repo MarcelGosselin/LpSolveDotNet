e4640733859db19cf18c72402863f5a3b52e48ba
Marcel Gosselin (gmail) <marcel.gosselin@gmail.com>
2020-02-18 01:21:57 -0500

    Prevent extra builds, cleanup old files

--- build/Build.cmd
+++ build/Build.cmd
@@ -5,15 +5,10 @@ pushd %~dp0
 cd /d "%THIS_DIR%..\src"
 
 echo.
-echo Build project
+echo Build project as though it is from GitHub workflows, to test nupkg
 ::dotnet pack --configuration Release -p:NoWarn=1591 || goto :error
 msbuild -noLogo -t:Clean;Pack -restore -p:Configuration=Release LpSolveDotNet.sln || goto :error
 
-::if [%1]==[push] (
-::	echo.
-::	echo Push package to NuGet
-::	nuget push "%OUTPUT_DIR%\LpSolveDotNet.%TARGET_VERSION%.nupkg" -Source https://www.nuget.org || goto :error
-::)
 goto :end
 
 :error
